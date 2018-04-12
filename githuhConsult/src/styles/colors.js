ef12daea5bc695c8e450c73ce8e56c954ac521db
Matheus Medeiros <matheuslinkm@gmail.com>
2018-04-12 01:53:21 -0300

    cria os estilos globais da aplicação

---
+++ githuhConsult/src/styles/colors.js
@@ -0,0 +1,11 @@
+export default {
+    darkgray: '#EEEEEE',
+    white: '#FFFFFF',
+    darket: '#333333',
+    regular: '999999',
+    dark: '666666',
+
+    transparent: 'transparent',
+    whiteTransparent: 'rgba(255, 255, 255, 0.5)',
+    darkTransparent: 'rgba(0, 0, 0, 0.5)',
+};
